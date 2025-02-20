a62f2bef4ef13f459db6af0c31328726914886ab
JjungminLee <julie0964@naver.com>
2025-02-20 14:21:07 +0900

    design: color,font 세팅

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+final class WeveColor {
+  static final main = _MainColors();
+  static final bg = _BgColors();
+}
+
+final class _MainColors {
+  _MainColors();
+
+  final Color orange1 = Color(0xFFF26F3A);
+  final Color blue1 = Color(0xFF345FAD);
+  final Color pink1 = Color(0xFFEE9CC4);
+  final Color yellow1 = Color(0xFFF8C811);
+  final Color green1 = Color(0xFF05A552);
+  final Color orange2 = Color(0XFF791C0A);
+  final Color blue2 = Color(0XFF051042);
+  final Color pink2 = Color(0XFF781B4E);
+  final Color yellow2 = Color(0XFF873F19);
+  final Color green2 = Color(0XFF005227);
+}
+
+final class _BgColors {
+  _BgColors();
+  final Color bg1 = Color(0XFFFFFBEE);
+  final Color bg2 = Color(0XFFECEAE9);
+  final Color bg3 = Color(0XFFEDE6DB);
+  final Color bg4 = Color(0x33242424);
+}
---
+++ lib/core/constants/fonts.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class WeveText {
+  static TextStyle header1({Color color = Colors.black}) => TextStyle(
+        fontFamily: 'Pretendard',
+        fontSize: 32,
+        fontWeight: FontWeight.w700,
+        color: color,
+      );
+  static TextStyle header2({Color color = Colors.black}) => TextStyle(
+        fontFamily: 'Pretendard',
+        fontSize: 28,
+        fontWeight: FontWeight.w700,
+        color: color,
+      );
+  static TextStyle header3({Color color = Colors.black}) => TextStyle(
+      fontFamily: 'Pretendard',
+      fontSize: 24,
+      fontWeight: FontWeight.w700,
+      color: color);
+  static TextStyle header4({Color color = Colors.black}) => TextStyle(
+      fontFamily: 'Pretendard',
+      fontSize: 20,
+      fontWeight: FontWeight.w700,
+      color: color);
+  static TextStyle header5({Color color = Colors.black}) => TextStyle(
+      fontFamily: 'Pretendard',
+      fontSize: 16,
+      fontWeight: FontWeight.w700,
+      color: color);
+  static TextStyle body1({Color color = Colors.black}) => TextStyle(
+      fontFamily: 'Pretendard',
+      fontSize: 20,
+      fontWeight: FontWeight.w400,
+      color: color);
+  static TextStyle body2({Color color = Colors.black}) => TextStyle(
+      fontFamily: 'Pretendard',
+      fontSize: 18,
+      fontWeight: FontWeight.w400,
+      color: color);
+  static TextStyle body3({Color color = Colors.black}) => TextStyle(
+      fontFamily: 'Pretendard',
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+      color: color);
+  static TextStyle body4({Color color = Colors.black}) => TextStyle(
+      fontFamily: 'Pretendard',
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      color: color);
+  static TextStyle caption1({Color color = Colors.black}) => TextStyle(
+      fontFamily: 'Pretendard',
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      color: color);
+  static TextStyle caption2({Color color = Colors.black}) => TextStyle(
+      fontFamily: 'Pretendard',
+      fontSize: 8,
+      fontWeight: FontWeight.w400,
+      color: color);
+}
