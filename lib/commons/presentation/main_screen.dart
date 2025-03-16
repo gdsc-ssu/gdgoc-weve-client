fb321f52e6360480bd68d4fd116da9ee907f8904
Oliy <eunsoa0310@gmail.com>
2025-03-16 17:40:47 +0900

    feat: indexPage 구현
    - index_screen 에서 main_screen(junior)로 이동
    - mode_button의 가로길이를 화면 가로길이에 따라 유동적으로 렌더링되도록 변경

--- lib/commons/presentation/main_screen.dart
+++ lib/commons/presentation/main_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weve_client/commons/widgets/header/model/header_type.dart';
 import 'package:weve_client/commons/widgets/header/view/header_widget.dart';
 import 'package:weve_client/commons/widgets/header/viewmodel/header_viewmodel.dart';
+import 'package:weve_client/commons/widgets/mode_button/model/mode_type.dart';
 import 'package:weve_client/core/constants/colors.dart';
 import 'package:weve_client/core/navigator/senior_navigation_bar.dart';
 import 'package:weve_client/core/provider/StateNotifierProvider.dart';
@@ -11,21 +12,37 @@ import 'package:weve_client/features/junior/presentation/views/junior_my_screen.
 import 'package:weve_client/features/junior/presentation/views/junior_write_screen.dart';
 
 class MainScreen extends ConsumerWidget {
-  const MainScreen({super.key});
+  final ModeType? modeType;
+
+  const MainScreen({super.key, this.modeType});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final headerViewModel = ref.read(headerProvider.notifier);
     final selectedIndex = ref.watch(navigationProvider);
 
-    final List<Widget> pages = [
+    final List<Widget> juniorPages = [
       const JuniorHomeScreen(),
       const JuniorWriteScreen(),
       const JuniorMyScreen()
     ];
 
+    // 시니어 페이지는 아직 구현되지 않았으므로 임시로 주니어 페이지를 사용합니다.
+    final List<Widget> seniorPages = [
+      const JuniorHomeScreen(),
+      const JuniorWriteScreen(),
+      const JuniorMyScreen()
+    ];
+
+    final List<Widget> pages =
+        modeType == ModeType.senior ? seniorPages : juniorPages;
+
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      headerViewModel.setHeader(HeaderType.juniorTitleLogo, title: "나의 고민");
+      headerViewModel.setHeader(
+          modeType == ModeType.senior
+              ? HeaderType.seniorTitleLogo
+              : HeaderType.juniorTitleLogo,
+          title: "나의 고민");
     });
 
     return Scaffold(
--- lib/commons/widgets/mode_button/view/mode_button.dart
+++ lib/commons/widgets/mode_button/view/mode_button.dart
@@ -11,6 +11,15 @@ class ModeButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // 화면 너비를 가져옵니다.
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    // 버튼 너비 계산: (화면 너비 - 좌우 패딩 40px - 버튼 사이 간격 20px) / 2
+    final buttonWidth = (screenWidth - 60) / 2;
+
+    // 버튼 높이는 너비와 동일하게 설정하여 정사각형 형태를 유지합니다.
+    final buttonHeight = buttonWidth;
+
     return GestureDetector(
       onTap: () {
         Navigator.push(
@@ -19,8 +28,8 @@ class ModeButton extends StatelessWidget {
         );
       },
       child: Container(
-        width: 165,
-        height: 165,
+        width: buttonWidth,
+        height: buttonHeight,
         decoration: BoxDecoration(
           color: modeTypeModel.color,
           borderRadius: BorderRadius.circular(20),
