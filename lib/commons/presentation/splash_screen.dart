c955f6018ddbc027abc4f21b4192eab25778ae6c
Oliy <eunsoa0310@gmail.com>
2025-03-16 20:23:48 +0900

    fix: spash_screen에서 스플래시 텍스트를 juniorHeader로 변경

--- lib/commons/presentation/splash_screen.dart
+++ lib/commons/presentation/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:weve_client/commons/widgets/junior/header/view/header.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -7,32 +8,26 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          const Expanded(
-            child: Center(
-              child: Text(
-                'splash page',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Spacer(flex: 2),
+            const JuniorHeader(),
+            const Spacer(flex: 2),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 50.0),
+              child: SizedBox(
+                height: 110,
+                child: Lottie.asset(
+                  'assets/animations/weve_character_animation.json',
+                  fit: BoxFit.contain,
                 ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 50.0),
-            child: SizedBox(
-              height: 110,
-              child: Lottie.asset(
-                'assets/animations/weve_character_animation.json',
-                fit: BoxFit.contain,
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
