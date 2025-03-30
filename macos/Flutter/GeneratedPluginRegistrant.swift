3a49611ae12032699b1c4edeb85eb11b6fcf1546
Oliy <eunsoa0310@gmail.com>
2025-03-30 12:48:55 +0900

    refactor: url_launcher 관련 플러그인 제거 및 등록 코드 삭제
    - pubspec.lock에서 url_launcher 및 관련 종속성 제거
    - 각 플랫폼의 플러그인 등록 코드에서 url_launcher 관련 코드 삭제

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,11 +8,9 @@ import Foundation
 import audioplayers_darwin
 import path_provider_foundation
 import speech_to_text
-import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
