62f4331c1770ca659e7ff9adb6de0f26ccfdbc12
Oliy <eunsoa0310@gmail.com>
2025-04-02 00:02:58 +0900

    refactor: 메인 화면에서 주니어 로그인 화면으로의 전환 로직 수정

--- lib/commons/presentation/main_screen.dart
+++ lib/commons/presentation/main_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weve_client/commons/widgets/mode_button/model/mode_type.dart';
-import 'package:weve_client/features/junior/presentation/views/junior_main_screen.dart';
+import 'package:weve_client/features/junior/presentation/views/login/junior_login_screen.dart';
 import 'package:weve_client/features/senior/presentation/views/login/senior_login_screen.dart';
 
 class MainScreen extends StatelessWidget {
@@ -14,7 +14,8 @@ class MainScreen extends StatelessWidget {
     if (modeType == ModeType.senior) {
       return const SeniorLoginScreen();
     } else {
-      return const JuniorMainScreen();
+      // TODO : MainScreen 에서 로그인 여부에 따라 화면 전환 처리 필요
+      return const JuniorLoginScreen();
     }
   }
 }
