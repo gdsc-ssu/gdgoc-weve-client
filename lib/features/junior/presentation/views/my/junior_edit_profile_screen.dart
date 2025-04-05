d4d257b26bf758e21abac8386365d63953b33154
Oliy <eunsoa0310@gmail.com>
2025-04-05 16:04:48 +0900

    fix: junior_edit_profile - dispose 메서드에서 ref 사용 제거 및 헤더 복원 로직 개선

--- lib/features/junior/presentation/views/my/junior_edit_profile_screen.dart
+++ lib/features/junior/presentation/views/my/junior_edit_profile_screen.dart
@@ -50,8 +50,7 @@ class _JuniorEditProfileScreenState
 
   @override
   void dispose() {
-    // 화면이 소멸될 때 콜백 제거
-    ref.read(headerProvider.notifier).clearBackPressedCallback();
+    // dispose에서 ref 사용 제거
     birthController.removeListener(_validateBirth);
     nameController.dispose();
     birthController.dispose();
@@ -173,7 +172,6 @@ class _JuniorEditProfileScreenState
 
   // 뒤로가기 버튼 처리를 위한 오버라이딩
   Future<bool> _onWillPop() async {
-    // 마이페이지로 돌아가기 전에 헤더를 원래대로 복원
     _restoreMyPageHeader();
     Navigator.pop(context);
     return false;
@@ -181,6 +179,9 @@ class _JuniorEditProfileScreenState
 
   // 마이페이지 헤더 복원
   void _restoreMyPageHeader() {
+    // 위젯이 이미 dispose 되었다면 실행하지 않음
+    if (!mounted) return;
+
     final locale = ref.read(localeProvider);
     final appLocalizations = AppLocalizations(locale);
 
